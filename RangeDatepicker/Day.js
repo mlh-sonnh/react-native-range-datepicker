2f702f43733b764497567965ba07ef8b1c4692cf
sonnh <sonnh@lifetimetech.vn>
2020-05-07 08:59:26 +0700

    add gradient

--- RangeDatepicker/Day.js
+++ RangeDatepicker/Day.js
@@ -1,93 +1,257 @@
-'use strict'
 import React from 'react';
-import {
-	View,
-	Text,
-	TouchableWithoutFeedback,
-	Dimensions
-} from 'react-native';
+import { View, Text, TouchableWithoutFeedback, Dimensions } from 'react-native';
 import moment from 'moment';
+import LinearGradient from 'react-native-linear-gradient';
 
 const DEVICE_WIDTH = Dimensions.get('window').width;
 
 export default class Day extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		if(nextProps.day.type == this.props.day.type)
-			return false;
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.day.type == this.props.day.type) return false;
 
-		return true;
-	}
+        return true;
+    }
 
-	render() {
-		let {day, dayProps} = this.props;
-		let dayStyle = {backgroundColor : 'transparent', position: 'relative', width: "14.28%"};
-		let textDayStyle = {color: 'black'};
+    render() {
+        const { day, dayProps } = this.props;
+        let dayStyle = {
+            backgroundColor: 'transparent',
+            position: 'relative',
+            width: '14.28%',
+        };
+        console.log(day.date, moment(day.date, 'YYYYMMDD').isoWeekday());
+        let textDayStyle = [6, 7].includes(
+            moment(day.date, 'YYYYMMDD').isoWeekday()
+        )
+            ? { color: 'red' }
+            : { color: 'black' };
 
-		switch(day.type){
-			case "single" : 
-				dayStyle = {backgroundColor : dayProps.selectedBackgroundColor, borderRadius: Math.floor(DEVICE_WIDTH / 7), width: "14.28%" }
-				textDayStyle = {color: dayProps.selectedTextColor};
-				break;
-			case "first" :
-				dayStyle = {backgroundColor : dayProps.selectedBackgroundColor, borderBottomLeftRadius: Math.floor(DEVICE_WIDTH / 7), borderTopLeftRadius: Math.floor(DEVICE_WIDTH / 7), width: "14.28%" }
-				textDayStyle = {color: dayProps.selectedTextColor};
-				break;
-			case "last" :
-				dayStyle = {backgroundColor : dayProps.selectedBackgroundColor, borderBottomRightRadius: Math.floor(DEVICE_WIDTH / 7), borderTopRightRadius: Math.floor(DEVICE_WIDTH / 7), width: "14.28%" }
-				textDayStyle = {color: dayProps.selectedTextColor};
-				break;
-			case "between" :
-				dayStyle = {backgroundColor : dayProps.selectedBackgroundColor, width: "14.28%"}
-				textDayStyle = {color: dayProps.selectedTextColor};
-				break;
-			case "disabled" :
-			case "blockout" :
-				textDayStyle = {color: '#ccc'};
-			default: break;
-		}
+        switch (day.type) {
+            case 'single':
+                dayStyle = {
+                    backgroundColor: dayProps.selectedBackgroundColor,
+                    borderRadius: Math.floor(DEVICE_WIDTH / 7),
+                    width: '14.28%',
+                };
+                textDayStyle = { color: dayProps.selectedTextColor };
+                break;
+            case 'first':
+                dayStyle = {
+                    backgroundColor: dayProps.selectedBackgroundColor,
+                    width: '14.28%',
+                };
+                textDayStyle = { color: dayProps.selectedTextColor };
+                break;
+            case 'last':
+                dayStyle = {
+                    backgroundColor: dayProps.selectedBackgroundColor,
+                    width: '14.28%',
+                };
+                textDayStyle = { color: dayProps.selectedTextColor };
+                break;
+            case 'between':
+                dayStyle = { backgroundColor: 'red', width: '14.28%' };
+                textDayStyle = { color: dayProps.selectedTextColor };
+                break;
+            case 'disabled':
+            case 'blockout':
+                textDayStyle = { color: '#ccc' };
+            default:
+                break;
+        }
 
-		if(day.date){
-			if(day.type == 'disabled')
-				return (
-					<TouchableWithoutFeedback activeOpacity={1} style={dayStyle}>
-						<View style={{...dayStyle, height: Math.floor(DEVICE_WIDTH / 7), justifyContent: 'center'}}>
-							<Text style={{...textDayStyle, textAlign: "center", backgroundColor: 'transparent', fontSize: Math.floor(DEVICE_WIDTH / 26)}}>{moment(day.date, 'YYYYMMDD').date()}</Text>
-							{day.date == moment().format("YYYYMMDD") ? (<View style={{position: 'absolute', top:0, bottom:0, left:0, right: 0, justifyContent: 'center', backgroundColor: 'transparent'}}><Text style={{fontSize: Math.floor(DEVICE_WIDTH / 17), fontWeight: 'bold', color: '#ccc', textAlign: 'center'}}>__</Text></View>) : null}
-						</View>
-					</TouchableWithoutFeedback>
-				);
-			else if(day.type == 'blockout') {
-				const strikeTop = Math.floor(DEVICE_WIDTH / -22);
-				return (
-					<TouchableWithoutFeedback activeOpacity={1} style={dayStyle}>
-						<View style={{...dayStyle, height: Math.floor(DEVICE_WIDTH / 7), justifyContent: 'center'}}>
-							<Text style={{...textDayStyle, textAlign: "center", backgroundColor: 'transparent', fontSize: Math.floor(DEVICE_WIDTH / 26)}}>{moment(day.date, 'YYYYMMDD').date()}</Text>
-							<View style={{position: 'absolute', top: strikeTop, bottom:0, left:0, right: 0, justifyContent: 'center', backgroundColor: 'transparent'}}><Text style={{fontSize: Math.floor(DEVICE_WIDTH / 17), color: '#ccc', textAlign: 'center'}}>__</Text></View>
-						</View>
-					</TouchableWithoutFeedback>
-				);
-			}
-			else
-				return (
-					<TouchableWithoutFeedback activeOpacity={1} style={dayStyle} onPress={() => this.props.onSelectDate(moment(day.date, 'YYYYMMDD'))}>
-						<View style={{...dayStyle, height: Math.floor(DEVICE_WIDTH / 7), justifyContent: 'center'}}>
-							<Text style={{...textDayStyle, textAlign: "center", backgroundColor: 'transparent', fontSize: Math.floor(DEVICE_WIDTH / 26)}}>{moment(day.date, 'YYYYMMDD').date()}</Text>
-							{day.date == moment().format("YYYYMMDD") ? (<View style={{position: 'absolute', top:0, bottom:0, left:0, right: 0, justifyContent: 'center', backgroundColor: 'transparent'}}><Text style={{fontSize: Math.floor(DEVICE_WIDTH / 17), fontWeight: 'bold', color: dayProps.selectedBackgroundColor, textAlign: 'center'}}>__</Text></View>) : null}
-						</View>
-					</TouchableWithoutFeedback>
-				);
-		}
-		else
-			return (
-				<TouchableWithoutFeedback activeOpacity={1} style={dayStyle}>
-					<View style={{...dayStyle, height: Math.floor(DEVICE_WIDTH / 7), justifyContent: 'center'}}>
-						<Text style={{ ...textDayStyle, textAlign: "center", backgroundColor: 'transparent', fontSize: Math.floor(DEVICE_WIDTH / 26)}}>{null}</Text>
-					</View>
-				</TouchableWithoutFeedback>
-			);
-	}
+        if (day.date) {
+            if (day.type == 'disabled') {
+                return (
+                    <TouchableWithoutFeedback
+                        activeOpacity={1}
+                        style={dayStyle}
+                    >
+                        <View
+                            style={{
+                                ...dayStyle,
+                                height: Math.floor(DEVICE_WIDTH / 7),
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    ...textDayStyle,
+                                    textAlign: 'center',
+                                    backgroundColor: 'transparent',
+                                    fontSize: Math.floor(DEVICE_WIDTH / 26),
+                                }}
+                            >
+                                {moment(day.date, 'YYYYMMDD').date()}
+                            </Text>
+                            {day.date == moment().format('YYYYMMDD') ? (
+                                <View
+                                    style={{
+                                        position: 'absolute',
+                                        top: 0,
+                                        bottom: 0,
+                                        left: 0,
+                                        right: 0,
+                                        justifyContent: 'center',
+                                        backgroundColor: 'transparent',
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            fontSize: Math.floor(
+                                                DEVICE_WIDTH / 17
+                                            ),
+                                            fontWeight: 'bold',
+                                            color: '#ccc',
+                                            textAlign: 'center',
+                                        }}
+                                    >
+                                        __
+                                    </Text>
+                                </View>
+                            ) : null}
+                        </View>
+                    </TouchableWithoutFeedback>
+                );
+            }
+            if (day.type == 'blockout') {
+                const strikeTop = Math.floor(DEVICE_WIDTH / -22);
+                return (
+                    <TouchableWithoutFeedback
+                        activeOpacity={1}
+                        style={dayStyle}
+                    >
+                        <View
+                            style={{
+                                ...dayStyle,
+                                height: Math.floor(DEVICE_WIDTH / 10),
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    ...textDayStyle,
+                                    textAlign: 'center',
+                                    backgroundColor: 'transparent',
+                                    fontSize: Math.floor(DEVICE_WIDTH / 26),
+                                }}
+                            >
+                                {moment(day.date, 'YYYYMMDD').date()}
+                            </Text>
+                            <View
+                                style={{
+                                    position: 'absolute',
+                                    top: strikeTop,
+                                    bottom: 0,
+                                    left: 0,
+                                    right: 0,
+                                    justifyContent: 'center',
+                                    backgroundColor: 'transparent',
+                                }}
+                            >
+                                <Text
+                                    style={{
+                                        fontSize: Math.floor(DEVICE_WIDTH / 17),
+                                        color: '#ccc',
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    __
+                                </Text>
+                            </View>
+                        </View>
+                    </TouchableWithoutFeedback>
+                );
+            }
+            return (
+                <TouchableWithoutFeedback
+                    activeOpacity={1}
+                    style={dayStyle}
+                    onPress={() =>
+                        this.props.onSelectDate(moment(day.date, 'YYYYMMDD'))
+                    }
+                >
+                    <View
+                        style={{
+                            ...dayStyle,
+                            height: Math.floor(DEVICE_WIDTH / 10),
+                            justifyContent: 'center',
+                            maxHeight: 30,
+                        }}
+                    >
+                        <LinearGradient
+                            start={{ x: 0.0, y: 0 }}
+                            end={{ x: 1, y: 0 }}
+                            locations={[0.2, 0.8, 1]}
+                            colors={['red', 'green', 'white']}
+                        >
+                            <Text
+                                style={{
+                                    ...textDayStyle,
+                                    textAlign: 'center',
+                                    backgroundColor: 'transparent',
+                                    fontSize: Math.floor(DEVICE_WIDTH / 26),
+                                }}
+                            >
+                                {moment(day.date, 'YYYYMMDD').date()}
+                            </Text>
+                            {day.date == moment().format('YYYYMMDD') ? (
+                                <View
+                                    style={{
+                                        position: 'absolute',
+                                        top: -Math.floor(DEVICE_WIDTH / 8),
+                                        bottom: 0,
+                                        left: 0,
+                                        right: 0,
+                                        justifyContent: 'center',
+                                        backgroundColor: 'transparent',
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            fontSize: Math.floor(
+                                                DEVICE_WIDTH / 9
+                                            ),
+                                            fontWeight: 'bold',
+                                            color:
+                                                dayProps.selectedBackgroundColor,
+                                            textAlign: 'right',
+                                        }}
+                                    >
+                                        .
+                                    </Text>
+                                </View>
+                            ) : null}
+                        </LinearGradient>
+                    </View>
+                </TouchableWithoutFeedback>
+            );
+        }
+        return (
+            <TouchableWithoutFeedback activeOpacity={1} style={dayStyle}>
+                <View
+                    style={{
+                        ...dayStyle,
+                        height: Math.floor(DEVICE_WIDTH / 10),
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Text
+                        style={{
+                            ...textDayStyle,
+                            textAlign: 'center',
+                            backgroundColor: 'transparent',
+                            fontSize: Math.floor(DEVICE_WIDTH / 26),
+                        }}
+                    >
+                        {null}
+                    </Text>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
 }
--- RangeDatepicker/Month.js
+++ RangeDatepicker/Month.js
@@ -1,134 +1,207 @@
-'use strict'
 import React from 'react';
-import {
-	View,
-	Text
-} from 'react-native';
-import DayRow from './DayRow'
-import moment from 'moment'
+import { View, Text } from 'react-native';
+import moment from 'moment';
+import DayRow from './DayRow';
 
 export default class Month extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	shouldComponentUpdate(nextProps, nextState) {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.minDate != this.props.minDate) return true;
 
-		if(nextProps.minDate != this.props.minDate)
-			return true;
+        if (nextProps.maxDate != this.props.maxDate) return true;
 
-		if(nextProps.maxDate != this.props.maxDate)
-			return true;
+        if (nextProps.availableDates != this.props.availableDates) return true;
 
-		if(nextProps.availableDates != this.props.availableDates)
-			return true;
+        if (
+            nextProps.startDate &&
+            nextProps.startDate.format('YYYYMM') == nextProps.month
+        )
+            return true;
 
-		if(nextProps.startDate && nextProps.startDate.format("YYYYMM") == nextProps.month)
-			return true;
+        if (
+            nextProps.untilDate &&
+            nextProps.untilDate.format('YYYYMM') == nextProps.month
+        )
+            return true;
 
-		if(nextProps.untilDate && nextProps.untilDate.format("YYYYMM") == nextProps.month)
-			return true;
+        if (
+            this.props.startDate &&
+            this.props.startDate.format('YYYYMM') == nextProps.month
+        )
+            return true;
 
-		if(this.props.startDate && this.props.startDate.format("YYYYMM") == nextProps.month)
-			return true;
+        if (
+            this.props.untilDate &&
+            this.props.untilDate.format('YYYYMM') == nextProps.month
+        )
+            return true;
 
-		if(this.props.untilDate && this.props.untilDate.format("YYYYMM") == nextProps.month)
-			return true;
+        if (
+            nextProps.startDate &&
+            nextProps.untilDate &&
+            nextProps.startDate.format('YYYYMM') < nextProps.month &&
+            nextProps.untilDate.format('YYYYMM') > nextProps.month
+        )
+            return true;
 
-		if(nextProps.startDate && nextProps.untilDate && nextProps.startDate.format("YYYYMM") < nextProps.month && nextProps.untilDate.format("YYYYMM") > nextProps.month)
-			return true;
+        if (
+            this.props.untilDate &&
+            this.props.startDate &&
+            this.props.startDate.format('YYYYMM') < nextProps.month &&
+            this.props.untilDate.format('YYYYMM') > nextProps.month
+        )
+            return true;
 
-		if(this.props.untilDate && this.props.startDate && this.props.startDate.format("YYYYMM") < nextProps.month && this.props.untilDate.format("YYYYMM") > nextProps.month)
-			return true;
+        return false;
+    }
 
-		return false;
-	}
+    getDayStack(month) {
+        const res = [];
+        const currMonth = moment(month).month(); // get this month
+        const currDate = moment(month).startOf('month'); // get first day in this month
 
+        let dayColumn = [];
+        const dayRow = [];
+        let dayObject = {};
+        const {
+            startDate,
+            untilDate,
+            availableDates,
+            minDate,
+            maxDate,
+            ignoreMinDate,
+        } = this.props;
 
-	getDayStack(month){
-		let res = [];
-		let currMonth = moment(month).month(); //get this month
-		let currDate = moment(month).startOf("month"); //get first day in this month
+        do {
+            dayColumn = [];
+            for (let i = 0; i < 7; i++) {
+                dayObject = {
+                    type: null,
+                    date: null,
+                };
+                if (i == currDate.days() && currDate.month() == currMonth) {
+                    if (
+                        minDate &&
+                        minDate.format('YYYYMMDD') &&
+                        currDate.format('YYYYMMDD') < minDate.format('YYYYMMDD')
+                    ) {
+                        if (
+                            startDate &&
+                            startDate.format('YYYYMMDD') >
+                                currDate.format('YYYYMMDD') &&
+                            currDate.format('YYYYMMDD') >
+                                moment().format('YYYYMMDD') &&
+                            ignoreMinDate
+                        ) {
+                        } else {
+                            dayObject.type = 'disabled';
+                        }
+                    }
+                    if (
+                        maxDate &&
+                        maxDate.format('YYYYMMDD') &&
+                        currDate.format('YYYYMMDD') > maxDate.format('YYYYMMDD')
+                    ) {
+                        dayObject.type = 'disabled';
+                    }
+                    if (
+                        availableDates &&
+                        availableDates.indexOf(currDate.format('YYYYMMDD')) ==
+                            -1
+                    ) {
+                        dayObject.type = 'blockout';
+                    }
+                    if (
+                        startDate &&
+                        startDate.format('YYYYMMDD') ==
+                            currDate.format('YYYYMMDD')
+                    ) {
+                        if (!untilDate) dayObject.type = 'single';
+                        else {
+                            dayObject.type = 'first';
+                        }
+                    }
+                    if (
+                        untilDate &&
+                        untilDate.format('YYYYMMDD') ==
+                            currDate.format('YYYYMMDD')
+                    ) {
+                        dayObject.type = 'last';
+                    }
+                    if (
+                        startDate &&
+                        startDate.format('YYYYMMDD') <
+                            currDate.format('YYYYMMDD') &&
+                        untilDate &&
+                        untilDate.format('YYYYMMDD') >
+                            currDate.format('YYYYMMDD')
+                    )
+                        dayObject.type = 'between';
 
-		let dayColumn = [];
-		let dayRow = [];
-		let dayObject = {};
-		let {startDate, untilDate, availableDates, minDate, maxDate, ignoreMinDate} = this.props;
+                    dayObject.date = currDate.clone().format('YYYYMMDD');
+                    dayColumn.push(dayObject);
+                    currDate.add(1, 'day');
+                } else {
+                    if (
+                        startDate &&
+                        untilDate &&
+                        startDate.format('YYYYMMDD') <
+                            currDate.format('YYYYMMDD') &&
+                        untilDate.format('YYYYMMDD') >=
+                            currDate.format('YYYYMMDD')
+                    )
+                        dayObject.type = 'between';
 
-		do{
-			dayColumn = [];
-			for(let i = 0; i < 7; i++){
-				dayObject = {
-					type : null,
-					date: null
-				};
-				if(i == currDate.days() && currDate.month() == currMonth)
-				{
-					if(minDate && minDate.format("YYYYMMDD") && currDate.format("YYYYMMDD") < minDate.format("YYYYMMDD")){
-						if(startDate && startDate.format('YYYYMMDD') > currDate.format("YYYYMMDD") && currDate.format("YYYYMMDD") > moment().format("YYYYMMDD") && ignoreMinDate){}
-						else{
-							dayObject.type = 'disabled';
-						}
-					}
-					if(maxDate && maxDate.format("YYYYMMDD") && currDate.format("YYYYMMDD") > maxDate.format("YYYYMMDD")){
-						dayObject.type = 'disabled';
-					}
-					if(availableDates && availableDates.indexOf(currDate.format("YYYYMMDD")) == -1){
-						dayObject.type = 'blockout';
-					}
-					if(startDate && startDate.format('YYYYMMDD') == currDate.format('YYYYMMDD')){
-						if(!untilDate)
-							dayObject.type = 'single';
-						else{
-							dayObject.type = 'first';
-						}
-					}
-					if(untilDate && untilDate.format('YYYYMMDD') == currDate.format('YYYYMMDD')){
-						dayObject.type = 'last';
-					}
-					if((startDate && startDate.format('YYYYMMDD') < currDate.format('YYYYMMDD')) && 
-						(untilDate && untilDate.format('YYYYMMDD') > currDate.format('YYYYMMDD')))
-						dayObject.type = 'between';
+                    dayColumn.push(dayObject);
+                }
+            }
 
-					dayObject.date = currDate.clone().format('YYYYMMDD');
-					dayColumn.push(dayObject);
-					currDate.add(1, 'day');
-				}
-				else{
-					if(startDate && untilDate &&
-						(
-							startDate.format('YYYYMMDD') < currDate.format('YYYYMMDD')  && 
-							untilDate.format('YYYYMMDD') >= currDate.format('YYYYMMDD')
-						)
-					)
-						dayObject.type = 'between';
+            dayRow.push(dayColumn);
+        } while (currDate.month() == currMonth);
 
-					dayColumn.push(dayObject);
-				}
-			}
+        return dayRow;
+    }
 
-			dayRow.push(dayColumn);
-		} while (currDate.month() == currMonth);
-
-		return dayRow;
-	}
-
-	render() {
-		const { month, dayProps } = this.props;
-		const dayStack = this.getDayStack(moment(month, 'YYYYMM'));
-		return (
-			<View>
-				<Text style={{fontSize: 14, padding: 14}}>{moment(month, 'YYYYMM').format("MMMM YYYY")}</Text>
-				<View>
-					{
-						dayStack.map((days, i) => {
-							return (
-								<DayRow days={days} dayProps={dayProps} key={i} onSelectDate={this.props.onSelectDate}/>
-							)
-						})
-					}
-				</View>
-			</View>
-		);
-	}
+    render() {
+        const { month, dayProps } = this.props;
+        const dayStack = this.getDayStack(moment(month, 'YYYYMM'));
+        return (
+            <View>
+                <Text
+                    style={{ fontSize: 14, padding: 14, alignSelf: 'center' }}
+                >
+                    {moment(month, 'YYYYMM').format('MMMM YYYY')}
+                </Text>
+                <View>
+                    {dayStack.map((days, i) => (
+                        <DayRow
+                            days={days}
+                            dayProps={dayProps}
+                            key={i}
+                            onSelectDate={this.props.onSelectDate}
+                        />
+                    ))}
+                </View>
+                <View>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Text>{'\u2022'}</Text>
+                        <Text style={{ flex: 1, paddingLeft: 5 }}>abc</Text>
+                    </View>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Text>{'\u2022'}</Text>
+                        <Text style={{ flex: 1, paddingLeft: 5 }}>xyz</Text>
+                    </View>
+                </View>
+                <View
+                    style={{
+                        borderBottomColor: 'grey',
+                        borderBottomWidth: 1,
+                    }}
+                />
+            </View>
+        );
+    }
 }
